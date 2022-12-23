79c7129ecccb450e380bbcb589bd07b8d56c8a9a
ShaneAdrianOpinion <120696891+ShaneAdrianOpinion@users.noreply.github.com>
2022-12-23 22:35:27 +0800

    Practical Final

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class App extends JFrame{
     private JPanel pnlMain;
     private JRadioButton rbCustomer;
     private JRadioButton rbClerk;
@@ -24,12 +26,38 @@ public class App {
     private List<Person> persons;
 
     public App() {
-        persons = new ArrayList<>();
+        ButtonGroup group = new ButtonGroup();
+        group.add(rbClerk);
+        group.add(rbCustomer);
+        group.add(rbManager);
+
         // TODO add implementations for all milestones here
+        persons = new ArrayList<>();
+        btnSayHi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    var input = String.valueOf(tfName.getText());
+                    JOptionPane.showMessageDialog(pnlMain, "Hi" + input);
+                } catch (IllegalArgumentException letterError){
+                    JOptionPane.showMessageDialog(pnlMain, "Do not input letters!");
+                }catch (Exception error){
+                    JOptionPane.showMessageDialog(pnlMain, "Input a positive integers");
+                }
+            }
+        });
+
     }
 
     public static void main(String[] args) {
         // add here how to make GUI visible
+        App opinion = new App();
+        opinion.setSize(600,500);
+        opinion.setTitle("Anything");
+        opinion.setContentPane(opinion.pnlMain);
+        opinion.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        opinion.setVisible(true);
+
     }
 
     static void giveReward(int n) {
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,6 @@
+
+
+
 public class Person {
     // TODO implement Person and its subclasses in other Java files
 }
